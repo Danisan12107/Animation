4e1008c7e27a57a4eef99dbde24f8764e0357002
danisan <danisan@uw.edu>
2025-01-16 17:34:16 -0800

    seeing if

--- megaman.js
+++ megaman.js
@@ -1,7 +1,7 @@
 class megaman {
     constructor(game) {
         this.game = game;
-        this.animator  = new Amimator(ASSET_MANAGER.getAsset("./megaman.png"), 0, 0, 180, 192, 4, 0.1);
+        this.animator  = new Amimator(ASSET_MANAGER.getAsset("./megaman.png"), 0, 0, 180, 192, 4, 0.5);
     };
 
     update() {
